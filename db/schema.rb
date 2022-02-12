197d9472e0b8e2b08d9871e72c0d7ad2da66c640
Elaine Wu <amsmm265@gmail.com>
2022-02-12 14:22:07 +0800

    add type_colunm and admin_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_064741) do
+ActiveRecord::Schema.define(version: 2022_02_12_052938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 2022_02_10_064741) do
     t.string "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "admin_id"
+    t.string "type"
+    t.index ["admin_id"], name: "index_courses_on_admin_id"
   end
 
   create_table "users", force: :cascade do |t|
