a5a0ada6f1828264a5a462674a0b0483da3e6524
Elaine Wu <amsmm265@gmail.com>
2022-02-15 12:04:03 +0800

    add_duration_validates

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,7 @@ class Course < ApplicationRecord
 
   enum currency: [:TWD, :USD, :JPY]
 
+  validates :name, presence: true
+  validates :duration, presence:true, numericality:{ less_than: 30 }
+
 end
