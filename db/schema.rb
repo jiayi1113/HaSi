781ae757b315c2590dcd3c4ddbbf749b0e2d12fb
Elaine Wu <amsmm265@gmail.com>
2022-02-15 12:03:14 +0800

    modify:course_column_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_122740) do
+ActiveRecord::Schema.define(version: 2022_02_15_032441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_122740) do
     t.string "state"
     t.string "url"
     t.string "describe"
-    t.string "date"
+    t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "admin_id"
