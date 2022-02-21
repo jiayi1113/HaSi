ab0e9ab211f7f5c200346a708922501dacc8f854
Elaine Wu <amsmm265@gmail.com>
2022-02-21 15:08:41 +0800

    change Enum

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :admin
 
-  enum currency: [:TWD, :USD, :JPY]
+  enum currency: {TWD: 0, USD: 1, JPY: 2}
 
   validates :name, presence: true
   validates :duration, presence:true, numericality:{ less_than: 30 }
